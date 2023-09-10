effdd262153e9d0fe58262ebdca3516ef881b3ce
Serhii Vostrikov <65418387+Antiserggio@users.noreply.github.com>
2023-09-11 01:43:52 +0300

    Summary

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,28 +2,26 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-
-console.log(galleryItems);
 const listGallery = document.querySelector(".gallery");
-console.log(listGallery);
 
 listGallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
-listGallery.addEventListener("click", handleClick);
 
 function createGalleryMarkup(items) {
   return items
     .map(
-      ({ original, preview, description }) =>
-        ` <li class="gallery__item">
-             <a class="gallery__link" href="${original} " onclick="event.preventDefault()">
-             <img
-             class="gallery__image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"
-             />
-             </a>
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}" >
+            <img class="gallery__image"
+            src="${preview}" alt="${description}" />
+            </a>
         </li>`
     )
     .join("");
 }
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
